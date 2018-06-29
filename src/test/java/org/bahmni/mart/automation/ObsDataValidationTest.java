62107412a08800a9397efdd6f61e00e8f3c5d643
pavanb018 <pavanb@thoughtworks.com>
2018-06-29 17:19:56 +0530

    Adding data validation code when multiselect is disabled

--- src/test/java/org/bahmni/mart/automation/ObsDataValidationTest.java
+++ src/test/java/org/bahmni/mart/automation/ObsDataValidationTest.java
@@ -72,7 +72,139 @@ public class ObsDataValidationTest {
         @Test
         public void validateObsFormsWithoutMultiSelect(){
 
-            validateObsFormsData(false, "formData.json", "formDataToBeVerified.json");
+            //validateObsFormsData(false, "formData.json", "formDataToBeVerified.json");
+
+            List<FormData> formDataList = FormInputDataJsonLoader.readFormDataFromJson("formData.json");
+            String formName;
+            Map<String, String> columnNameType = new HashMap<>();
+            List<FormData> FQDNDataList = ToBeVerifiedDataJSONLoader.readFormDataFromJson("formDataToBeVerified.json");
+            String tableName;
+            Map<String, String> FQDNfieldValueMap = new HashMap<>();
+            String restURL;
+            String jobExecutionId;
+            int formCount = 0;
+
+            NewPatientPage np = PageFactory.initElements(driver, NewPatientPage.class);
+            List<PatientData> patientDetailsList = PatientLoader.readFormDataFromJson();
+            String patientIQ = "";
+            int patientId = 0;
+
+            try {
+
+                restURL = new Utils().getRestURL();
+
+                SeleniumHelper.login(driver);
+                SeleniumHelper.goToNewPatientForm(driver);
+
+                for (PatientData patientData: patientDetailsList) {
+
+                    patientIQ = np.createPatient(patientData,driver);
+                    //np.goToHomePage(driver);
+                    patientId = Utils.getPatientIdentifier(openmrsConnection,patientIQ);
+
+                }
+
+                SeleniumHelper.goToProgramDashBoard(driver, patientIQ);
+                SeleniumHelper.goToConsultation(driver, patientIQ);
+
+
+                // For each Obs form declared in the input JSON file fill the data using selenium
+                // After filling the data and saving the form, also confirm that the data is correctly entered in the form
+
+                for (FormData formData : formDataList) {
+                    formName = formData.getFormName();
+                    System.out.println("Filling Data for Obs form from UI: " + formName);
+                    SeleniumHelper.fillFormData(driver, formName, formData);
+
+                }
+
+                // Once data is filled in Obs forms above, run mart using Rest and poll till job is completed successfully
+
+                jobExecutionId = RestHelper.startBatchJob(restURL, false);
+
+                if (RestHelper.pollUntilComplete(restURL, jobExecutionId) != null) {
+                //if (true) {
+                    System.out.println("mart Job is executed successfully");
+
+                    for (FormData formData : FQDNDataList) {
+                        ArrayList<String> multiselectTestData = new ArrayList<>();
+                        ArrayList<String> multiselectDBData = new ArrayList<>();
+                        Map<String, ArrayList<String >> dataMap = new HashMap<>();
+                        String[] multiselectColumnValues;
+
+                        // Fetch the input data that was entered to Obs form
+                        FQDNfieldValueMap = formData.getFieldValueMap();
+                        tableName = formData.getFormName();
+
+                        //From mart DB Fetch the column names and type ( int, str) of column along with data from the table for Obs form
+                        columnNameType = Utils.getTableColumnsAndTypes(postgresConnection, tableName);
+                        ResultSet rs = Utils.getTableData(postgresConnection, tableName, 2390);
+                        ResultSetMetaData rsmd = rs.getMetaData();
+                        int colcount = rsmd.getColumnCount();
+                        int i;
+                        System.out.println("Column Count is: "+ colcount);
+
+                        ArrayList<String> nonMultiDBData = new ArrayList<>();
+
+                        while (rs.next()) {
+
+                            for (String columnName : FQDNfieldValueMap.keySet()) {
+                                ArrayList<String> nonMultiTestData = new ArrayList<>();
+
+                                if (columnName.endsWith("_multiselect")) {
+
+                                    // First fetch the multiselect values into a list
+                                    multiselectColumnValues = FQDNfieldValueMap.get(columnName).split(";");
+
+                                    // Now remove the _multiselect suffix from the column name so that we can get the column values from DB
+                                    columnName = columnName.split("_multiselect")[0];
+
+                                    for (String word : multiselectColumnValues) {
+                                        System.out.println(word);
+                                        if (!multiselectTestData.contains(word)) {
+                                            multiselectTestData.add(word);
+                                        }
+                                    }
+                                    //System.out.println("Data from DB for multi: " + rs.getString(columnName));
+                                    if (!multiselectDBData.contains(rs.getString(columnName))) {
+                                        multiselectDBData.add(rs.getString(columnName));
+                                    }
+                                }
+                                else {
+                                    String columnData;
+                                    columnData = rs.getString(columnName);
+                                    if (columnData != null && !nonMultiTestData.contains(columnData)) {
+                                        nonMultiTestData.add(columnData);
+                                    }
+                                    if (!nonMultiTestData.isEmpty()) {
+                                        dataMap.put(columnName, nonMultiTestData);
+                                    }
+                                    //System.out.println("Column: " + columnName + " has data: " + columnData);
+                                    System.out.println("Verifying column: " + columnName + ". str Data from DB: " + columnData + " Data from test: " + FQDNfieldValueMap.get(columnName));
+                                }
+                            }
+                        }
+
+                        Collections.sort(multiselectTestData);
+                        Collections.sort(multiselectDBData);
+
+                        System.out.println("Data from test file: " + multiselectTestData);
+                        System.out.println("Data from DB: "+ multiselectDBData);
+
+                        assertEquals("The data validation for multiselect Table: " + tableName + " failed." + "multiselect value from Test: " + multiselectTestData + " " + "data values from mart: " + multiselectDBData, multiselectTestData, multiselectDBData );
+
+                        for (String column: dataMap.keySet()) {
+                            assertEquals("The data validation for multiselect Table: " + tableName + " failed." + "non-multiselect value from Test: " + FQDNfieldValueMap.get(column) + " " + "data values from mart: " + dataMap.get(column), FQDNfieldValueMap.get(column), dataMap.get(column).get(0));
+                        }
+
+                    }
+                    System.out.print("Number of columns is: " + columnNameType.size());
+                }
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
 
         }
 
